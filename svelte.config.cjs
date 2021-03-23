ef7c0353ce9ba140a75d4b2731bbcabffce78155
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-03-23 21:45:53 +0000

    netlify-adapter change

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,6 @@
 // const node = require('@sveltejs/adapter-node');
-// const adapterNetlify = require('@sveltejs/adapter-netlify');
-const adapterStatic = require('@sveltejs/adapter-static');
+const adapterNetlify = require('@sveltejs/adapter-netlify');
+// const adapterStatic = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,7 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: adapterStatic(),
+		adapter: adapterNetlify(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
