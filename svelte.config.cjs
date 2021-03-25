e31b69ee46437c7b4278fc44469482aa394b3a10
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-03-25 16:50:33 +0000

    change adapter to vercel

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,6 @@
 // const node = require('@sveltejs/adapter-node');
-const adapterNetlify = require('@sveltejs/adapter-netlify');
+// const adapterNetlify = require('@sveltejs/adapter-netlify');
+const vercel = require('@sveltejs/adapter-vercel');
 // const adapterStatic = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
@@ -10,7 +11,7 @@ module.exports = {
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
 		// adapter: adapterNetlify(),
-		adapter: adapterNetlify(),
+		adapter: vercel(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
