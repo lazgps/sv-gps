117e48e1233bc600726fdb39872146470a943523
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-03-25 19:25:58 +0000

    more changes to post.js

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -1,15 +1,17 @@
-import fs from 'fs';
+import { readdirSync, readFileSync } from 'fs';
 import fm from 'front-matter';
-// import matter from 'gray-matter';
 import marked from 'marked';
-// let postDir = '../../../posts';
-let postDir = `./static/posts`;
+
+let basedir = process.cwd();
+let postDir = basedir + '/static/posts';
+
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 marked.setOptions({
     gfm: true,
     breaks: true
 });
+
 
 function appendLeadZ(n) {
     if( n <=9) {
@@ -18,8 +20,8 @@ function appendLeadZ(n) {
     return n
 }
 
-const posts = fs.readdirSync(postDir).map(postFilename => {
-    const postContent = fs.readFileSync(`${postDir}/${postFilename}`, {
+const posts = readdirSync(postDir).map(postFilename => {
+    const postContent = readFileSync(`${postDir}/${postFilename}`, {
         encoding: 'utf8'
     });
     const postfm = fm(postContent);
@@ -38,18 +40,5 @@ const posts = fs.readdirSync(postDir).map(postFilename => {
 posts.forEach(post => {
     post.html = post.html.replace(/^\t{3}/gm, '');
 });
-
-/* const posts = [];
-let postFile = fs.readdirSync(postDir);
-postFile.forEach(postFile => {
-    const fContents = fs.readFileSync(`${postDir}/${postFile}`, 'utf-8');
-    const { body, ...frontMatter } = fm(fContents);
-    const slug = postFile.split('.').slice(0, -1).join('.');
-    const html = marked(body);
-    // posts.push( slug, { ...frontMatter.attributes, html });
-    posts.push(Object.create(slug, frontMatter.attributes));
-}) */
-// console.log(posts);
-// console.log(Object.keys(posts));
 
 export default posts;
