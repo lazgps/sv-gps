d60f5429b30b6d38b44ffd36c254d45618f6a63e
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-04-29 14:33:39 +0000

    revised modals

--- src/lib/Modal1.svelte
+++ src/lib/Modal1.svelte
@@ -1,4 +1,31 @@
 <script>
+  import { createForm } from 'svelte-forms-lib';
+  import * as yup from 'yup';
+  import Email from './smtp.js';
+
+  const { form, errors, handleChange, handleSubmit, handleReset } =  createForm({
+    initialValues: {
+      firstname: '',
+      lastname: '',
+      email: '',
+      phone: '',
+      numofprops: '',
+      goals: ''
+    },
+    validationSchema: yup.object().shape({
+      firstname: yup.string().required(),
+      lastname: yup.string().required(),
+      email: yup.string().email().required(),
+      phone: yup.string().required(),
+      numofprops: yup.number().required(),
+      goals: yup.string()
+    }),
+    onSubmit: values => {
+      show();
+      handleReset();
+    }
+  });
+
   let showMdl = false;
 
   export function show() {
@@ -10,64 +37,91 @@
 {#if showMdl}
 <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
     <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-      <!--
-        Background overlay, show/hide based on modal state.
-  
-        Entering: "ease-out duration-300"
-          From: "opacity-0"
-          To: "opacity-100"
-        Leaving: "ease-in duration-200"
-          From: "opacity-100"
-          To: "opacity-0"
-      -->
       <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
-  
       <!-- This element is to trick the browser into centering the modal contents. -->
       <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-  
-      <!--
-        Modal panel, show/hide based on modal state.
-  
-        Entering: "ease-out duration-300"
-          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          To: "opacity-100 translate-y-0 sm:scale-100"
-        Leaving: "ease-in duration-200"
-          From: "opacity-100 translate-y-0 sm:scale-100"
-          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-      -->
       <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
         <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
           <div class="text-center text-lg text-gray-900 mb-2">
             <h2>Submit the Form to Obtain a Free Rental Analysis</h2>
           </div>
-          <form action="#" method="POST">
+          <form on:submit|preventDefault="{handleSubmit}">
             <div class="col-span-6 sm:col-span-3">
-              <label for="first_name" class="block text-sm font-medium text-gray-700">First name:</label>
-              <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <label for="firstname" class="block text-sm font-medium text-gray-700">First name:</label>
+              <input name="firstname"
+                     id="firstname" 
+                     class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                     on:change={handleChange}
+                     bind:value={$form.firstname}
+              />
+              {#if $errors.firstname}
+                <small>{$errors.firstname}</small>
+              {/if}
             </div>
             <div class="col-span-6 sm:col-span-3">
-              <label for="last_name" class="block text-sm font-medium text-gray-700">Last name:</label>
-              <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <label for="lastname" class="block text-sm font-medium text-gray-700">Last name:</label>
+              <input name="lastname" 
+                     id="lastname"
+                     class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                     on:change={handleChange}
+                     bind:value={$form.lastname} 
+              />
+              {#if $errors.lastname}
+                <small>{$errors.lastname}</small>
+              {/if}
             </div>
 
             <div class="col-span-6 sm:col-span-3">
-              <label for="email_address" class="block text-sm font-medium text-gray-700">Email address:</label>
-              <input type="text" name="email_address" id="email_address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <label for="email" class="block text-sm font-medium text-gray-700">Email address:</label>
+              <input name="email" 
+                     id="email" 
+                     class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                     on:change={handleChange}
+                     bind:value={$form.email}
+              />
+              {#if $errors.email}
+                <small>{$errors.email}</small>
+              {/if}
             </div>
 
             <div class="col-span-6 sm:col-span-3">
-              <label for="phone_number" class="block text-sm font-medium text-gray-700">Phone Number:</label>
-              <input type="text" name="phone_number" id="phone_number" autocomplete="phone-number" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <label for="phone" class="block text-sm font-medium text-gray-700">Phone Number:</label>
+              <input name="phone" 
+                     id="phone" 
+                     class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                     on:change={handleChange}
+                     bind:value={$form.phone}
+              />
+              {#if $errors.phone}
+                <small>{$errors.phone}</small>
+              {/if}
             </div>
 
             <div class="col-span-6 sm:col-span-3">
-              <label for="number_properties" class="block text-sm font-medium text-gray-700">Number of Properties to Manage:</label>
-              <input type="text" name="number_properties" id="number_properties" autocomplete="number-properties" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+              <label for="numofprops" class="block text-sm font-medium text-gray-700">Number of Properties to Manage:</label>
+              <input name="numofprops"
+                     id="numofprops"
+                     class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" 
+                     on:change={handleChange}
+                     bind:value={$form.numofprops}
+              />
+              {#if $errors.numofprops}
+                <small>{$errors.numofprops}</small>
+              {/if}
             </div>
 
             <div class="mt-1 col-span-6 sm:col-span-3">
-              <label for="regoals" class="block text-sm font-medium text-gray-700">What are your Property Management Goals?</label>
-              <textarea id="regoals" name="regoals" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Your Property Management Goals" />
+              <label for="goals" class="block text-sm font-medium text-gray-700">What are your Property Management Goals?</label>
+              <textarea id="goals" 
+                        name="goals"
+                        rows="3" 
+                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Your Property Management Goals" 
+                        on:change={handleChange}
+                        bind:value={$form.goals}
+              />
+              {#if $errors.goals} 
+                <small>{$errors.goals}</small>
+              {/if}
             </div>
             <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
               <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
