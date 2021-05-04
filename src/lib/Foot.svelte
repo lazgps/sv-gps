bea481fc92b2dbaa6862424659adf41f1a4fc11d
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-05-04 16:07:26 -0400

    footer grid work

--- src/lib/Foot.svelte
+++ src/lib/Foot.svelte
@@ -2,13 +2,15 @@
 <footer class="bg-red">
     <div class="container grid grid-cols-2 grid-flow-row justify-items-auto">
         <div class="grid grid-cols-1 grid-flow-row justify-items-auto md:mx-8 md:my-4 md:py-4 md:px-8 text-white">
-            <p class="md:mr-6 md:pr-6">838 Grand Regency Pointe</p>
-            <p class="md:mr-6 md:pr-6">Altamonte Springs, FL 32714</p>
-            <p class="md:mr-6 md:pr-6">407-715-2652</p>
-            <p class="md:mr-6 md:pr-6">info@gps-realestate.com</p>
+            <p>838 Grand Regency Pointe</p>
+            <p>Altamonte Springs, FL 32714</p>
+            <p>407-715-2652</p>
+            <p>info@gps-realestate.com</p>
         </div>
         <div class="grid grid-cols-2 grid-flow-row justify-items-auto md:mx-8 md:my-4 md:py-4 md:px-8">
-            <p>Affiliations:</p>
+            <div class="justify-self-center text-white">
+                <p>Affiliations:</p>
+            </div>
             <div class="md:mr-6 md:pr-6">
                 <img class="h-12" src="/images/logos/equal-house-opp.png" alt="Equal Housing logo">
             </div>
