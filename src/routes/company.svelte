54f4ffc226154934f4958ee74fb95544550112b5
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-05-04 17:10:18 -0400

    company file changes

--- src/routes/company.svelte
+++ src/routes/company.svelte
@@ -13,7 +13,7 @@
       <div class="flex flex-wrap -m-4">
         <div class="p-4 lg:w-1/2">
           <div class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-            <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="/images/staff/Tere-Striped-shit-hip-hand.jpg">
+            <img alt="team" class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="/images/staff/Tere-Striped-shirt-hip-hand.jpg">
             <div class="flex-grow sm:pl-8">
               <h2 class="title-font font-medium text-lg text-gray-900">Teresa Alvarez</h2>
               <h3 class="text-gray-500 mb-3">Principal Broker</h3>
