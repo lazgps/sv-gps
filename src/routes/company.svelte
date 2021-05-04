d5983a889cba1a4c34e603972eacc531065b4550
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-05-04 12:58:58 -0400

    changes as per doc

--- src/routes/company.svelte
+++ src/routes/company.svelte
@@ -1,4 +1,10 @@
+<script>
+  import Modal1 from '$lib/Modal1.svelte';
+  let Mdl1;
+</script>
+
 <main class="text-gray-600 body-font">
+    <Modal1 bind:this={Mdl1}></Modal1>
     <div class="container px-5 py-24 mx-auto">
       <div class="flex flex-col text-center w-full mb-20">
         <h1 class="text-2xl font-medium title-font mb-4 text-gray-900 tracking-widest">OUR TEAM</h1>
@@ -45,7 +51,10 @@
             <div class="flex-grow sm:pl-8">
               <h2 class="title-font font-medium text-lg text-gray-900">Todd Cerny</h2>
               <h3 class="text-gray-500 mb-3">Designer</h3>
-              <p class="mb-4">Todd is a proud graduate of the University of Central Florida and a long-term resident of the Orlando metropolitan area. Todd was lucky enough to graduate UCF and start and continue his career in the Orlando area. This has allowed him to create long lasting relationships in business and with friends in our local area. His experience in the fortune 500 technology industry, including social media and project management, brings a special skill set to GPS. In his spare time Todd enjoys UCF football, travelling, and playing guitar.</p>
+              <p class="mb-4">Todd is a proud graduate of the University of Central Florida and a long-term resident of the Orlando metropolitan area. 
+                Todd was lucky enough to graduate UCF and start and continue his career in the Orlando area. This has allowed him to create long lasting relationships in business and with friends in our local area. 
+                His experience in the fortune 500 technology industry, including social media and project management, brings a special skill set to GPS. In his spare time Todd enjoys UCF football, 
+                travelling, and playing guitar.</p>
               <span class="inline-flex">
                 <a class="text-gray-500">
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
@@ -72,7 +81,8 @@
             <div class="flex-grow sm:pl-8">
               <h2 class="title-font font-medium text-lg text-gray-900">Lazaro Cubas</h2>
               <h3 class="text-gray-500 mb-3">Accountant</h3>
-              <p class="mb-4">Lazaro is a U.S. Coast Guard veteran with years of experience in the technology, finance, and accounting sectors. Lazaro offers a unique skillset to our property management and real estate Customers through his thorough knowledge of our innovative technology, and deep understanding of our accounting systems. Lazaro is a key team member making sure your GPS technology and accounting experience is seamless, accurate, and carefree.</p>
+              <p class="mb-4">Lazaro is a U.S. Coast Guard veteran with years of experience in the technology, finance, and accounting sectors. Lazaro offers a unique skillset to our property management and real estate Customers through his thorough knowledge of our innovative technology, and deep understanding of our accounting systems. 
+                Lazaro is a key team member making sure your GPS technology and accounting experience is seamless, accurate, and carefree.</p>
               <span class="inline-flex">
                 <a class="text-gray-500">
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
@@ -99,7 +109,12 @@
             <div class="flex-grow sm:pl-8">
               <h2 class="title-font font-medium text-lg text-gray-900">Doraliz Fernandez</h2>
               <h3 class="text-gray-500 mb-3">Designer</h3>
-              <p class="mb-4">Doraliz Fernandez was born in Dominican Republic but raised most of her life in central florida. Coming from a very close family, she has learned to value relations both personal and professionally. Her value for family life has helped her develop traits and qualities such as flexibility, compassion, and perseverance which projects in every aspect of her work life. In 2015 she was offered the opportunity to shift occupations from hospitality to property management, working directly with HOA's. The knowledge and years of experience she was able to gain during this time gave her a smooth transition to Real Estate, once she joined the GPS Realty and Property Management family in 2018. During her personal time, Doraliz enjoys spending time with her husband, and their little girl GIanna. When she is not occupied at home, you can find her outdoors searching for the next adventure!</p>
+              <p class="mb-4">Doraliz Fernandez was born in Dominican Republic but raised most of her life in central florida. Coming from a very close family, 
+                she has learned to value relations both personal and professionally. Her value for family life has helped her develop traits and qualities such as flexibility, 
+                compassion, and perseverance which projects in every aspect of her work life. In 2015 she was offered the opportunity to shift occupations from hospitality to property management, 
+                working directly with HOA's. The knowledge and years of experience she was able to gain during this time gave her a smooth transition to Real Estate, once she joined the GPS Realty and Property Management family in 2018. 
+                During her personal time, Doraliz enjoys spending time with her husband, and their little girl GIanna. When she is not occupied at home, 
+                you can find her outdoors searching for the next adventure!</p>
               <span class="inline-flex">
                 <a class="text-gray-500">
                   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
@@ -120,6 +135,15 @@
             </div>
           </div>
         </div>
+        <div class="bg-gray-200 mx-auto flex px-5 py-10 items-center justify-center flex-row">
+          <div class="text-center lg:w-2/3 w-full">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Let's get your journey started to stress-free property management.</h1>
+            <h2 class="text-xl font-bold text-gray-900 mb-8 leading-relaxed">(407)-715-2652</h2>
+            <button class="mx-auto mt-4 text-white bg-red-500 border-0 py-2 px-4 focus:outline-none hover:bg-red-600 rounded text-bold text-xl" on:click={() => Mdl1.show()}>
+              Let's Begin
+            </button>
+          </div>
+      </div>
       </div>
     </div>
   </main>
--- src/routes/vacancies.svelte
+++ src/routes/vacancies.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from "svelte";
+    import Modal1 from '$lib/Modal1.svelte';
 
     let turnerData = [];
 
@@ -8,9 +9,11 @@
         turnerData = await rest.json();
     });
 
+    let Mdl1;
 </script>
 
 <main class="text-gray-600 body-font pb-12">
+    <Modal1 bind:this={Mdl1}></Modal1>
     <div class="container px-5 py-14 mx-auto">
       <div class="flex flex-wrap -m-4">
         {#each turnerData as data}       
@@ -37,6 +40,14 @@
             </div>
         {/each}
       </div>
+    </div>
+    <div class="flex-grow bg-gray-200 mt-10 mx-auto px-5 py-10 items-center justify-center">
+        <div class="text-center md:w-2/3 lg:w-full w-full">
+          <h2 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Let our simplified pricing structure fuel your profits</h2>
+          <button class="mx-auto mt-4 text-white bg-red-500 border-0 py-2 px-4 focus:outline-none hover:bg-red-600 rounded text-bold text-xl" on:click={() => Mdl.show()}>
+            Begin Here
+          </button>
+        </div>
     </div>
 </main>
 
