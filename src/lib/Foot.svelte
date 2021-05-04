ccaba51b68c55b69c16b1c53c2c1c36db004549a
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-05-04 16:11:51 -0400

    footer more grid work

--- src/lib/Foot.svelte
+++ src/lib/Foot.svelte
@@ -2,13 +2,13 @@
 <footer class="bg-red">
     <div class="container grid grid-cols-2 grid-flow-row justify-items-auto">
         <div class="grid grid-cols-1 grid-flow-row justify-items-auto md:mx-8 md:my-4 md:py-4 md:px-8 text-white">
-            <p>838 Grand Regency Pointe</p>
-            <p>Altamonte Springs, FL 32714</p>
-            <p>407-715-2652</p>
-            <p>info@gps-realestate.com</p>
+            <p>838 Grand Regency Pointe<br>   
+            Altamonte Springs, FL 32714<br>
+            407-715-2652<br>
+            info@gps-realestate.com</p>
         </div>
         <div class="grid grid-cols-2 grid-flow-row justify-items-auto md:mx-8 md:my-4 md:py-4 md:px-8">
-            <div class="justify-self-center text-white">
+            <div class="justify-self-center place-self-center self-center text-white">
                 <p>Affiliations:</p>
             </div>
             <div class="md:mr-6 md:pr-6">
