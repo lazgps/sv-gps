b5f4ebb7856f50012ca1205a3e84255c7bd09eb5
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-05-04 15:38:11 -0400

    more color changes

--- src/lib/Foot.svelte
+++ src/lib/Foot.svelte
@@ -1,8 +1,10 @@
 
-<footer class="bg-red-500">
+<footer class="bg-red">
     <div class="container flex flex-col items-center">
         <div>
-            <h2></h2>
+            <h2>838 Grand Regency Pointe</h2>
+            <h3>Altamonte Springs, FL 32714</h3>
+            
         </div>
         <div class="grid grid-cols-2 grid-flow-row justify-items-auto md:mx-8 md:my-4 md:py-4 md:px-8">
             <div class="md:mr-6 md:pr-6">
