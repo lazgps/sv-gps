ebeb27c745d3e08908641694474b0379d9cc7b0a
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-05-05 09:18:18 -0400

    changes in index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,7 +21,7 @@
     <div class="flex flex-col md:flex-row items-center mx-auto px-auto py-16 md:py-16 w-4/5">
       <div class="inline-flex flex-col items-center text-center lg:flex-grow lg:pr-24 md:pr-16 w-1/2 md:items-start md:text-left md:px-16">
         <h1 class="title-font sm:text-4xl text-3xl mb-2 tracking-tighter font-medium text-gray-900">Let Us Fuel Your Profits</h1>
-        <p class="mb-2 leading-relaxed text-2xl">We offer Top Tier Services and Easy, Simple Pricing</p>
+        <p class="mb-2 leading-relaxed">We offer Top Tier Services and Easy, Simple Pricing</p>
         <a class="text-red title-font text-bold text-2xl" href="/services">
           Here's how
         </a>
