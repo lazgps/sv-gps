7da481e1754f864a08a6a93febb28e477f7188b7
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-05-06 10:57:46 -0400

    index and footer changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,23 +18,23 @@
           </button>
         </div>
       </div>
-    <div class="flex flex-col md:flex-row items-center mx-auto px-auto py-16 md:py-16 w-4/5">
-      <div class="inline-flex flex-col items-center text-center lg:flex-grow lg:pr-24 md:pr-16 w-1/2 md:items-start md:text-left md:px-16">
+    <div class="flex flex-col-reverse md:flex-row items-center mx-auto px-auto lg:py-16 py-10 sm:py-10 w-4/5">
+      <div class="inline-flex flex-col items-center text-center lg:flex-grow lg:pr-24 md:pr-16 sm:2/3 lg:w-1/2 md:items-start md:text-left md:px-16">
         <h1 class="title-font sm:text-4xl text-3xl mb-2 tracking-tighter font-medium text-gray-900">Let Us Fuel Your Profits</h1>
         <p class="mb-2 leading-relaxed">We offer Top Tier Services and Easy, Simple Pricing</p>
         <a class="text-red title-font text-bold text-2xl" href="/services">
           Here's how
         </a>
       </div>
-      <div class="flex-grow lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <div class="flex-grow lg:max-w-lg lg:w-1/2 sm:w-2/3">
         <img class="object-cover object-center rounded" alt="hero" src="/images/stock-photos/Let_Us_Maximize_Your_Profits.jpg">
       </div>
     </div>
-    <div class="flex flex-col items-center md:flex-row mx-auto px-auto py-16 md:py-16 w-4/5">
-        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+    <div class="flex flex-col items-center md:flex-row mx-auto px-auto lg:py-16 py-10 sm:py-10 w-4/5">
+        <div class="lg:max-w-lg lg:w-1/2 sm:w-2/3">
           <img class="object-cover object-center rounded" alt="hero" src="/images/stock-photos/Innovative_Technology.jpg">
         </div>
-        <div class="inline-flex flex-col items-center text-center lg:flex-grow lg:pl-24  md:pl-16 w-1/2 md:items-start md:text-left">
+        <div class="inline-flex flex-col items-center text-center lg:flex-grow lg:pl-24 sm:pl-16 sm:w-2/3 lg:w-1/2 lg:items-start lg:text-left">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Use Our Innovative Technology to Your Advantage
           </h1>
           <p class="mb-8 leading-relaxed">We offer innovative technology to Owners and Tenants that can be used anywhere in real-time via the GPS Owner and Tenant portal. Find out how our technology can assist you</p>
@@ -43,15 +43,15 @@
           </button>
         </div>
     </div>
-    <div class="flex flex-col md:flex-row items-center mx-auto px-auto py-16 md:py-16 w-4/5">
-      <div class="inline-flex flex-col items-center text-center lg:flex-grow lg:pr-24 md:pr-16 w-1/2 md:items-start md:text-left md:px-16">
-        <h1 class="title-font sm:text-4xl text-3xl mb-4 tracking-tighter font-medium text-gray-900">Your Full Service Property Management Partner</h1>
-        <p class="mb-8 leading-relaxed">Are you a Realtor looking to secure future listings?  Take advantage of our "Real Estate Agent Referral Program"</p>
+    <div class="flex flex-col-reverse md:flex-row items-center mx-auto px-auto lg:py-16 py-10 sm:py-10 w-4/5">
+      <div class="inline-flex flex-col items-center text-center lg:flex-grow lg:pr-24 sm:pr-16 sm:2/3 lg:w-1/2 md:items-start md:text-left md:px-16">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 tracking-tighter font-medium text-gray-900">A Realtors Full Service Property Management Partner</h1>
+        <p class="mb-8 leading-relaxed">Are you a Realtor looking for a Property Manager or property management partner?  Take advantage of our <i>Real Estate Agent Referral Program</i></p>
         <button class="text-red-600 title-font text-bold text-2xl" on:click={() => Mdl2.show()}>
-          Begin Here
+          Realtors Begin Here
         </button>
       </div>
-      <div class="flex-grow lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <div class="flex-grow lg:max-w-lg lg:w-1/2 sm:2/3">
         <img class="object-cover object-center rounded" alt="hero" src="/images/stock-photos/Your_Full_Service_Property_Management_Partner.jpg">
       </div>
     </div>
