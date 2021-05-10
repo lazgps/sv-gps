5f993057c318bea93af3f79b821b5238b7e6e71a
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-05-09 22:35:08 -0400

    changes to app based on framework changes

--- mdsvex.config.cjs
+++ mdsvex.config.cjs
@@ -1,6 +1,19 @@
 module.exports = {
-    extensions: ['.svx', '.md'],
-    smartypants: {
-        dashes: 'oldschool',
-    }
+	extensions: [".svelte.md", ".md", ".svx"],
+	smartypants: {
+		dashes: "oldschool",
+	},
+	remarkPlugins: [
+		[require("remark-github"), {
+			// Use your own repository
+			repository: "https://github.com/svelte-add/mdsvex.git",
+		}],
+		require("remark-abbr"),
+	],
+	rehypePlugins: [
+		require("rehype-slug"),
+		[require("rehype-autolink-headings"), {
+			behavior: "wrap",
+		}],
+	],
 };
--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,23 @@
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config';
-import preprocess  from 'svelte-preprocess';
+import { mdsvex } from "mdsvex";
+import { mdsvexConfig } from "./mdsvex.config.js";
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	extensions: [".svelte", ...mdsvexConfig.extensions],
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
 	preprocess: [
-		preprocess(),
-		mdsvex(mdsvexConfig)
+		mdsvex(mdsvexConfig),
+		preprocess({
+			postcss: true
+		}),
 	],
-	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		// adapter: adapterNetlify(),
-		adapter: adapter(options),
 
+	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(),
 		target: '#svelte'
 	}
 };
