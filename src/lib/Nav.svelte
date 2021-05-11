29136775ecca158de5708eaa23e34f414cbb72dd
Lazaro <me@lazaro.dev>
2021-05-11 16:18:49 -0400

     changes to nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -26,10 +26,10 @@
         {/each}
     </ul>
     <div class="grid lg:auto-cols-auto grid-rows-1 grid-flow-col sm:grid-flow-row md:grid-flow-col lg:grid-flow-col justify-items-start sm:justify-items-start md:justify-items-end lg:justify-items-end ml-5 lg:ml-0">
-        <button class="hover:text-gray-800" on:click={showLogin}>Sign in</button>
+        <button class="hover:text-gray-800" on:click={showLogin}>Sign in ▾</button>
     </div>
     {#if lMenu}
-        <div class="origin-top-right absolute right-0 mt-12 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+        <div class="origin-top-right relative lg:absolute right-0 lg:mt-12 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
             <!-- Active: "bg-gray-100", Not Active: "" -->
             <a href="https://app.propertyware.com/pw/portals/gpsrealtypropertymanagementllc/owner.action" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Owner Login</a>
             <a href="https://app.propertyware.com/pw/portals/gpsrealtypropertymanagementllc/tenant.action" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Tenant Login</a>
